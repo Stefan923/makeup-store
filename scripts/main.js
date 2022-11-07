e947158985cf91eae7057a07951510037e8afdc3
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-11-07 20:58:54 +0200

    Reworked product cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const API_URL = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type='
+const DEFAULT_DESCRIPTION = 'No description was provided.'
 
 const lastActive = null
 
@@ -29,8 +30,12 @@ function loadProducts(data) {
                 <img class='card-image' src='${ product.image_link }'>
                 <div class='card-header'>
                     <h3>${ product.name }</h3>
-                    <span class='card-price'>${ product.price_sign }${ product.price }</span>
-                    <p class='card-description'>${ product.description }</p>
+                    <span class='card-price'>${ product.price_sign ? product.price_sign : "$" }${ product.price ? product.price : "0.0" }</span>
+                    <div class='card-description'>
+                        <p>${ product.description ? product.description : DEFAULT_DESCRIPTION }</p>
+                        <p>Category: ${ product.product_type ? product.product_type : "unknown" }</p>
+                        <p>Brand: ${ product.brand ? product.brand : "unknown" }</p>
+                    </div>
                 </div>
             </div>
         `)
