e13c1b6db654fcf488a5b812fe63b674326945b2
Ovidiu Stefan POPESCU <stefanpopescu923@gmail.com>
2022-11-07 17:16:52 +0200

    Rework product cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 const API_URL = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type='
 
+const lastActive = null
+
 $(window).bind("load", function() {
     $('.nav-btn').on('click', function() {
-        console.log('test')
+        $("a").removeClass("active")
+        $(this).addClass("active")
+
         requestProducts($(this).attr('category'))
     })
 })
@@ -18,7 +22,17 @@ function requestProducts(category) {
 
 function loadProducts(data) {
     console.log(data)
+    $('#products').empty()
     data.forEach(function(product) {
-        $('#products').append(`<div class='item center'><img class='item-image' src='${ product.image_link }'><p>${ product.name }</p></div>`)
+        $('#products').append(`
+            <div class='card center'>
+                <img class='card-image' src='${ product.image_link }'>
+                <div class='card-header'>
+                    <h3>${ product.name }</h3>
+                    <span class='card-price'>${ product.price_sign }${ product.price }</span>
+                    <p class='card-description'>${ product.description }</p>
+                </div>
+            </div>
+        `)
     })
 }
