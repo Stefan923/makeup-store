17db93de69e621981149da6bbd9f23ce799b8938
Ovidiu Stefan POPESCU <stefanpopescu923@gmail.com>
2022-11-08 16:30:13 +0200

    Added product card apply/mark /confirm/cancel buttons

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,17 @@ $(window).bind("load", function() {
         $('a').removeClass('active')
         $(this).addClass('active')
 
-        requestProducts($(this).attr('category'))
         $('#load-wrapper').show()
+        requestProducts($(this).attr('category'))
     })
+
+    $('#products').removeClass('container')
+    $('#products').addClass('text-box')
+
+    $('#products').append(`
+        <p>Lista de produse este goala.</p>
+        <p>Selectati o categorie din meniu pentru a vedea lista de produse.
+    `)
 
     $('#load-wrapper').hide()
 })
@@ -26,10 +34,16 @@ function requestProducts(category) {
 
 function loadProducts(data) {
     console.log(data)
+
+    $('#products').removeClass('container')
+    $('#products').removeClass('text-box')
+    $('#products').addClass('container')
     $('#products').empty()
+
+    let index = 0;
     data.forEach(function(product) {
         $('#products').append(`
-            <div class='card center'>
+            <div id='card-${ index }' class='card center'>
                 <img class='card-image' src='${ product.image_link }'>
                 <div class='card-header'>
                     <h3>${ product.name }</h3>
@@ -39,10 +53,61 @@ function loadProducts(data) {
                         <p>Category: ${ product.product_type ? product.product_type : "unknown" }</p>
                         <p>Brand: ${ product.brand ? product.brand : "unknown" }</p>
                     </div>
+                    <div class='flex-container'>
+                        <button class='btn btn-primary' onClick='showConfirmationCard(${ index }, "apply")'>Apply</button>
+                        <button class='btn btn-outline' onClick='showConfirmationCard(${ index }, "mark")'>Mark</button>
+                    </div>
+                </div>
+                <div id='card-confirmation-${ index }' class='card-confirmation'>
+                    <div class='flex-column'>
+                        <div><button class='btn btn-primary' onClick='confirmCard(${ index })'>Confirm</button></div>
+                        <div><button class='btn btn-outline' onClick='hideConfirmationCard(${ index })'>Cancel</button></div>
+                    </div>
                 </div>
             </div>
         `)
+        ++index;
     })
 
+    if (data.length == 0) {
+        $('#products').removeClass('container')
+        $('#products').addClass('text-box')
+        $('#products').append(`
+            <p>Lista de produse este goala.</p>
+            <p>Selectati o categorie din meniu pentru a vedea lista de produse.
+        `)
+    }
+
     $('#load-wrapper').hide()
+}
+
+function confirmCard(index) {
+    if ($(`#card-${ index }`).hasClass('apply')) {
+        applyCard(index)
+    } else {
+        markCard(index)
+    }
+}
+
+function applyCard(index) {
+    $(`#card-${ index }`).removeClass('card-marked')
+    $(`#card-${ index }`).addClass('card-applied')
+    hideConfirmationCard(index, 'apply')
+}
+
+function markCard(index) {
+    $(`#card-${ index }`).removeClass('card-applied')
+    $(`#card-${ index }`).addClass('card-marked')
+    hideConfirmationCard(index)
+}
+
+function showConfirmationCard(index, className) {
+    $(`#card-confirmation-${ index }`).addClass('card-show')
+    $(`#card-${ index }`).addClass(className)
+}
+
+function hideConfirmationCard(index) {
+    $(`#card-confirmation-${ index }`).removeClass('card-show')
+    $(`#card-${ index }`).removeClass('apply')
+    $(`#card-${ index }`).removeClass('mark')
 }
